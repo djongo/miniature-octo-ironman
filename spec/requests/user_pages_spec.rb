3feb266dbc07cdccbbde38d9783105051048d620
Djongo <djongo@urik>
2013-04-08 18:08:25 +0200

    DRY rspec requests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "UserPages" do
 		before { visit signup_path }
 
 		it { should have_selector('h1', text: 'Sign up')}
-		it { should have_selector('title', text: 'Sign up')}
+		it { should have_title('Sign up')}
 	end
 
 	describe "profile page" do
@@ -15,7 +15,7 @@ describe "UserPages" do
 		before { visit user_path(user) }
 		
 		it { should have_selector('h1', text: user.name) }
-		it { should have_selector('title', text: user.name) }
+		it { should have_title(user.name) }
 	end
 
 	describe "signup" do
@@ -30,7 +30,7 @@ describe "UserPages" do
 			describe "after submission" do
 				before { click_button submit }
 
-				it { should have_selector('title', text: 'Sign up') }
+				it { should have_title('Sign up') }
 				it { should have_content('error') }
 				# Could create test case per error
 			end
@@ -38,25 +38,25 @@ describe "UserPages" do
 		end
 
 		describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+	      before do
+	        fill_in "Name",         with: "Example User"
+	        fill_in "Email",        with: "user@example.com"
+	        fill_in "Password",     with: "foobar"
+	        fill_in "Confirmation", with: "foobar"
+	      end
 
-      it "should create a user"	do
-      	expect { click_button submit }.to change(User, :count).by(1)
-      end
+	      it "should create a user"	do
+	      	expect { click_button submit }.to change(User, :count).by(1)
+	      end
 
-      describe "after saving the user" do
-      	before { click_button submit }
-      	let(:user) { User.find_by_email('user@example.com') }
+	      describe "after saving the user" do
+	      	before { click_button submit }
+	      	let(:user) { User.find_by_email('user@example.com') }
 
-      	it { should have_selector('title', text: user.name) }
-      	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-		it { should have_link('Sign out') }      	
-      end
+	      	it { should have_title(user.name) }
+	      	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			it { should have_link('Sign out') }      	
+	      end
 		end
 	end
 end
